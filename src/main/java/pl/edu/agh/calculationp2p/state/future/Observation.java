947712a416334dcdd20da18f1fddd8e12d5a95a8
Marcin Szubert <marszub@gmail.com>
2021-12-13 17:18:17 +0100

    Observation implementataion

--- src/main/java/pl/edu/agh/calculationp2p/state/future/Observation.java
+++ src/main/java/pl/edu/agh/calculationp2p/state/future/Observation.java
@@ -1,4 +1,21 @@
 package pl.edu.agh.calculationp2p.state.future;
 
+import pl.edu.agh.calculationp2p.state.task.TaskRecord;
+
 public class Observation {
+    private final TaskRecord task;
+    private final Future<Observation> nextObservation;
+
+    public Observation(TaskRecord task, Future<Observation> nextObservation) {
+        this.task = task;
+        this.nextObservation = nextObservation;
+    }
+
+    public TaskRecord getTask(){
+        return task;
+    }
+
+    public Future<Observation> getNextObservation(){
+        return nextObservation;
+    }
 }
