0c44e83a060136bb5861330f62e3afec2dfdd651
Marcin Szubert <marszub@gmail.com>
2021-12-13 16:05:40 +0100

    extract interface from Scheduler

--- src/main/java/pl/edu/agh/calculationp2p/state/Scheduler.java
+++ src/main/java/pl/edu/agh/calculationp2p/state/Scheduler.java
@@ -2,39 +2,6 @@ package pl.edu.agh.calculationp2p.state;
 
 import pl.edu.agh.calculationp2p.state.request.MethodRequest;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public class Scheduler implements Runnable {
-    BlockingQueue<MethodRequest> activationQueue;
-    Servant servant;
-    Thread thread;
-
-    public Scheduler(Servant servant){
-        activationQueue = new LinkedBlockingQueue<>();
-        this.servant = servant;
-
-        thread = new Thread(this);
-    }
-
-    @Override
-    public void run() {
-        while(true){
-            try {
-                dispatch();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return;
-            }
-        }
-    }
-
-    public void enqueue(MethodRequest request) throws InterruptedException {
-        activationQueue.put(request);
-    }
-
-    private void dispatch() throws InterruptedException {
-        MethodRequest request = activationQueue.take();
-        request.call(servant);
-    }
+public interface Scheduler {
+    void enqueue(MethodRequest request) throws InterruptedException;
 }
