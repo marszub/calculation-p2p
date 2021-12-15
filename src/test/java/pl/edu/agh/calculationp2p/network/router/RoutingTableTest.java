de9a2b54b1281a9a2ba76cda6c4a301ec17f74e2
JanuszJakubiec <54719269+JanuszJakubiec@users.noreply.github.com>
2021-12-15 20:57:16 +0100

    Fixing tests

--- src/test/java/pl/edu/agh/calculationp2p/network/router/RoutingTableTest.java
+++ src/test/java/pl/edu/agh/calculationp2p/network/router/RoutingTableTest.java
@@ -1,7 +1,6 @@
 package pl.edu.agh.calculationp2p.network.router;
 import org.junit.jupiter.api.Test;
 import pl.edu.agh.calculationp2p.message.Message;
-import pl.edu.agh.calculationp2p.network.connection.Connection;
 import pl.edu.agh.calculationp2p.network.utilities.DummyConnection;
 import pl.edu.agh.calculationp2p.network.utilities.DummyMessage;
 
@@ -17,7 +16,7 @@ public class RoutingTableTest {
         RoutingTableImpl routingTable = new RoutingTableImpl();
         int id = 10;
         routingTable.addInterface(id);
-        assertTrue(routingTable.getInterfaces().containsKey(id));
+        assertTrue(routingTable.interfaceListContains(id));
     }
 
     @Test
@@ -27,27 +26,16 @@ public class RoutingTableTest {
         int id = 10;
         routingTable.addInterface(id);
         routingTable.removeInterface(id);
-        assertFalse(routingTable.getInterfaces().containsKey(id));
+        assertFalse(routingTable.interfaceListContains(id));
     }
 
     @Test
-    void checkIfConnectionBindsCorrectly()
+    void checkIfDummyConnectionTrySendsMessage()
     {
         RoutingTableImpl routingTable = new RoutingTableImpl();
         int id = 10;
         routingTable.addInterface(id);
-        Connection connection = new DummyConnection(true);
-        routingTable.bind(id, connection);
-        assertEquals(connection, routingTable.getInterfaces().get(id));
-    }
-
-    @Test
-    void checkIfConnectionTrySendsMessage()
-    {
-        RoutingTableImpl routingTable = new RoutingTableImpl();
-        int id = 10;
-        routingTable.addInterface(id);
-        Connection connection = new DummyConnection(true);
+        DummyConnection connection = new DummyConnection(true);
         routingTable.bind(id, connection);
         assertTrue(routingTable.trySend(id, new DummyMessage()));
         assertFalse(routingTable.trySend(id, new DummyMessage()));
@@ -61,8 +49,8 @@ public class RoutingTableTest {
         Message notPassing = new DummyMessage();
         List<Message> list = new LinkedList<>();
         list.add(notPassing);
-        addIdAndMakeMessageNotToBeSent(routingTable, id, list);
-        assertEquals(notPassing, routingTable.getMessageInterfaceQueue().get(id).pop());
+        addIdAndMakeMessageNotToBeSent(new DummyConnection(true), routingTable, id, list);
+
     }
 
     @Test
@@ -75,14 +63,15 @@ public class RoutingTableTest {
         List<Message> list = new LinkedList<>();
         list.add(notPassing1);
         list.add(notPassing2);
-        addIdAndMakeMessageNotToBeSent(routingTable, id, list);
+        DummyConnection connection = new DummyConnection(true);
+        addIdAndMakeMessageNotToBeSent(connection, routingTable, id, list);
         routingTable.resendAll();
-        assertEquals(1, routingTable.getMessageInterfaceQueue().get(id).size());
-        assertEquals(notPassing2, routingTable.getMessageInterfaceQueue().get(id).pop());
+        assertEquals(3 , connection.getList().size());
+        assertEquals(notPassing1 , connection.getLastMessage());
     }
 
     @Test
-    void checkIfResendAllResendsAllManyConnections()
+    void checkIfResendAllResendsAllManyDummyConnections()
     {
         RoutingTableImpl routingTable = new RoutingTableImpl();
         int id1 = 10;
@@ -97,13 +86,15 @@ public class RoutingTableTest {
         list.add(notPassing2);
         list2.add(notPassing3);
         list2.add(notPassing4);
-        addIdAndMakeMessageNotToBeSent(routingTable, id1, list);
-        addIdAndMakeMessageNotToBeSent(routingTable, id2, list2);
+        DummyConnection con1 = new DummyConnection(true);
+        DummyConnection con2 = new DummyConnection(true);
+        addIdAndMakeMessageNotToBeSent(con1, routingTable, id1, list);
+        addIdAndMakeMessageNotToBeSent(con2, routingTable, id2, list2);
         routingTable.resendAll();
-        assertEquals(1, routingTable.getMessageInterfaceQueue().get(id1).size());
-        assertEquals(notPassing2, routingTable.getMessageInterfaceQueue().get(id1).pop());
-        assertEquals(1, routingTable.getMessageInterfaceQueue().get(id2).size());
-        assertEquals(notPassing4, routingTable.getMessageInterfaceQueue().get(id2).pop());
+        assertEquals(3 , con1.getList().size());
+        assertEquals(notPassing1 , con1.getLastMessage());
+        assertEquals(3 , con2.getList().size());
+        assertEquals(notPassing3 , con2.getLastMessage());
     }
 
     @Test
@@ -120,12 +111,17 @@ public class RoutingTableTest {
         list.add(notPassing2);
         list.add(notPassing3);
         list.add(notPassing4);
-        addIdAndMakeMessageNotToBeSent(routingTable, id1, list);
+        DummyConnection connection = new DummyConnection(true);
+        addIdAndMakeMessageNotToBeSent(connection, routingTable, id1, list);
         routingTable.resendAll();
-        assertEquals(3, routingTable.getMessageInterfaceQueue().get(id1).size());
-        assertEquals(notPassing3, routingTable.getMessageInterfaceQueue().get(id1).pop());
-        assertEquals(notPassing4, routingTable.getMessageInterfaceQueue().get(id1).pop());
-        assertEquals(notPassing2, routingTable.getMessageInterfaceQueue().get(id1).pop());
+        connection.setResult(true);
+        routingTable.resendAll();
+        connection.setResult(true);
+        routingTable.resendAll();
+        assertEquals(7, connection.getList().size());
+        assertEquals(notPassing2, connection.getList().removeLast());
+        assertEquals(notPassing3, connection.getList().removeLast());
+        assertEquals(notPassing1, connection.getList().removeLast());
     }
 
     @Test
@@ -182,10 +178,9 @@ public class RoutingTableTest {
         assertTrue(routingTable.interfaceListContains(1));
     }
 
-    private void addIdAndMakeMessageNotToBeSent(RoutingTableImpl routingTable, int id, List<Message> messageList)
+    private void addIdAndMakeMessageNotToBeSent(DummyConnection connection, RoutingTableImpl routingTable, int id, List<Message> messageList)
     {
         routingTable.addInterface(id);
-        Connection connection = new DummyConnection(true);
         routingTable.bind(id, connection);
         for(Message message : messageList)
         {
