361466337604252cd076696bb84032b16f306db8
Kamil <61463685+CyberCommunism@users.noreply.github.com>
2021-12-29 00:23:20 +0100

    implemented calculated body

--- src/main/java/pl/edu/agh/calculationp2p/message/body/Calculated.java
+++ src/main/java/pl/edu/agh/calculationp2p/message/body/Calculated.java
@@ -1,36 +1,42 @@
 package pl.edu.agh.calculationp2p.message.body;
 
 import pl.edu.agh.calculationp2p.calculation.TaskResult;
-import pl.edu.agh.calculationp2p.message.Message;
 import pl.edu.agh.calculationp2p.message.MessageImpl;
 import pl.edu.agh.calculationp2p.message.process.MessageProcessContext;
 import pl.edu.agh.calculationp2p.network.router.Router;
 import pl.edu.agh.calculationp2p.state.future.Future;
 import pl.edu.agh.calculationp2p.state.task.TaskRecord;
+import pl.edu.agh.calculationp2p.state.task.TaskState;
 
 public class Calculated implements Body{
-    private int taskId;
-    private TaskResult taskResult;
+    private final int taskId;
+    private final TaskResult taskResult;
     public Calculated(int taskId, TaskResult taskResult){
         this.taskId = taskId;
         this.taskResult = taskResult;
     }
     @Override
     public String serializeType() {
-        throw new UnsupportedOperationException("Will be implemented");
+        return "\"calculated\"";
     }
 
     @Override
     public String serializeContent() {
-        return null;
+        String result = "";
+        result = result.concat("{\"task_id\":");
+        result = result.concat(String.valueOf(this.taskId));
+        result = result.concat(",\"result\":");
+        result = result.concat(taskResult.serialize());
+        result = result.concat("}");
+        return result;
     }
 
     @Override
     public void process(int sender, MessageProcessContext context) {
-        Future<TaskRecord> f = context.getStateUpdater().calculate(taskId, sender, taskResult);
-        context.getFutureProcessor().addFutureProcess(f, () -> {
+        Future<TaskRecord> calculateFuture = context.getStateUpdater().calculate(taskId, sender, taskResult);
+        context.getFutureProcessor().addFutureProcess(calculateFuture, () -> {
             Router router = context.getRouter();
-            router.send((Message) new MessageImpl(router.getId(), sender, new Confirm(f.get())));
+            router.send(new MessageImpl(router.getId(), sender, new Confirm(taskId, TaskState.Calculated, sender, calculateFuture.get())));
         });
     }
 
