81e4062736db23596d90e25ba3563cc679156578
Kamil <61463685+CyberCommunism@users.noreply.github.com>
2022-01-04 00:02:32 +0100

    Convert TaskStateMess to record

--- src/main/java/pl/edu/agh/calculationp2p/message/utils/TaskStateMess.java
+++ src/main/java/pl/edu/agh/calculationp2p/message/utils/TaskStateMess.java
@@ -2,20 +2,10 @@ package pl.edu.agh.calculationp2p.message.utils;
 
 import pl.edu.agh.calculationp2p.state.task.TaskState;
 
-public class TaskStateMess {
+public record TaskStateMess(int taskId, TaskState state, Integer owner) {
 
-    private int taskId;
-    private TaskState state;
-    private Integer owner;
-
-    public TaskStateMess(int taskId, TaskState state, Integer owner){
-        this.taskId = taskId;
-        this.state = state;
-        this.owner = owner;
-    }
-
-    public String serialize(){
-        return "{\"task_id\":"+this.taskId+",\"state\":"+this.state+",\"owner\":"+this.owner+",\"result\":\"null\"}";
+    public String serialize() {
+        return "{\"task_id\":" + this.taskId + ",\"state\":" + this.state + ",\"owner\":" + this.owner + ",\"result\":\"null\"}";
     }
 
 }
