44537313e4e749cdb48670da8b1d904984e4c3ca
Kamil <61463685+CyberCommunism@users.noreply.github.com>
2022-01-04 14:05:06 +0100

    Update NodeRegister.java
    Repaired time manage in NodeRegister

--- src/main/java/pl/edu/agh/calculationp2p/message/process/NodeRegister.java
+++ src/main/java/pl/edu/agh/calculationp2p/message/process/NodeRegister.java
@@ -1,54 +1,65 @@
 package pl.edu.agh.calculationp2p.message.process;
 
 import java.net.InetSocketAddress;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NodeRegister {
-
     private final int validityTime;
     private final Map<Integer, InetSocketAddress> publicNodes;
     private final List<Integer> privateNodes;
-    private final Map<Integer, Integer> allNodes;
+    private final Map<Integer, Long> allNodes;
 
     protected NodeRegister(int validityTime){
         this.allNodes = new HashMap<>();
+
         this.publicNodes = new HashMap<>();
         this.privateNodes = new ArrayList<>();
+
         this.validityTime = validityTime;
     }
 
     protected List<Integer> getOutdatedNodes(){
         List<Integer> result = new ArrayList<>();
         for(Integer nodeId : this.allNodes.keySet()){
-            if(this.allNodes.get(nodeId)==0){
+            if(now() - this.allNodes.get(nodeId) > validityTime){
                 result.add(nodeId);
             }
         }
-        for(Integer nodeId : this.allNodes.keySet()){
-            this.allNodes.replace(nodeId, 0);
-        }
-        // TODO: delete those nodes from here
+        result.forEach(nodeId -> {
+            this.allNodes.remove(nodeId);
+            this.publicNodes.remove(nodeId);
+            this.privateNodes.remove(nodeId);
+
+        });
         return result;
     }
+
+
     public void addPublicNode(int id, InetSocketAddress ip){
-        this.allNodes.put(id, 1);
+        this.allNodes.put(id, now());
         this.publicNodes.put(id, ip);
     }
     public void addPrivateNode(int id){
-        this.allNodes.put(id, 1);
+        this.allNodes.put(id, now());
         this.privateNodes.add(id);
     }
     public void updateNode(int id){
-        this.allNodes.replace(id, 1);
+        this.allNodes.replace(id, now());
     }
+
+
+
     public Map<Integer, InetSocketAddress> getPublicNodes(){
         return this.publicNodes;
     }
     public List<Integer> getPrivateNodes(){
         return this.privateNodes;
     }
-
+    private long now(){
+        return ZonedDateTime.now().toInstant().toEpochMilli();
+    }
 }
