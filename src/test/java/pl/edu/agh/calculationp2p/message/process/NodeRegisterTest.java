1fa84ea5f3eabb5185af4956e54d008d5dfc18a5
Kamil <61463685+K4m1lP@users.noreply.github.com>
2022-01-18 20:30:42 +0100

    HeartBeatEmiter and NodeRegister Tests

--- src/test/java/pl/edu/agh/calculationp2p/message/process/NodeRegisterTest.java
+++ src/test/java/pl/edu/agh/calculationp2p/message/process/NodeRegisterTest.java
@@ -2,31 +2,69 @@ package pl.edu.agh.calculationp2p.message.process;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NodeRegisterTest {
 
     @Test
     void getOutdatedNodes() {
-    }
 
-    @Test
-    void addPublicNode() {
-    }
+        int validityTime = 1000;
 
-    @Test
-    void addPrivateNode() {
+        NodeRegister nodeRegister = new NodeRegister(validityTime);
+
+        nodeRegister.addPrivateNode(1);
+        nodeRegister.addPrivateNode(2);
+        nodeRegister.addPrivateNode(3);
+
+        nodeRegister.addPublicNode(4, new InetSocketAddress("127.0.0.1", 2137));
+
+        List<Integer> outdated = nodeRegister.getOutdatedNodes();
+        assertEquals(outdated, new ArrayList<>());
+
+        try {
+            sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        outdated = nodeRegister.getOutdatedNodes();
+        assertEquals(outdated, new ArrayList<>(List.of(1,2,3,4)));
+
     }
 
     @Test
     void updateNode() {
+
+        int validityTime = 1000;
+
+        NodeRegister nodeRegister = new NodeRegister(validityTime);
+
+        nodeRegister.addPrivateNode(1);
+        nodeRegister.addPrivateNode(2);
+        nodeRegister.addPrivateNode(3);
+
+        nodeRegister.addPublicNode(4, new InetSocketAddress("127.0.0.1", 2137));
+
+        List<Integer> outdated = nodeRegister.getOutdatedNodes();
+        assertEquals(outdated, new ArrayList<>());
+
+        try {
+            sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        nodeRegister.updateNode(2);
+        outdated = nodeRegister.getOutdatedNodes();
+        assertEquals(outdated, new ArrayList<>(List.of(1,3,4)));
+
     }
 
-    @Test
-    void getPublicNodes() {
-    }
 
-    @Test
-    void getPrivateNodes() {
-    }
 }
