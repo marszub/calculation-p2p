82f5d41fd4e4580b70194408ff8f21f7e3148a9e
Marcin Szubert <marszub@gmail.com>
2022-01-31 18:40:15 +0100

    Long task

--- src/main/java/pl/edu/agh/calculationp2p/calculationTask/hashBreaking/HashBreakerFactory.java
+++ src/main/java/pl/edu/agh/calculationp2p/calculationTask/hashBreaking/HashBreakerFactory.java
@@ -5,11 +5,11 @@ import pl.edu.agh.calculationp2p.calculationTask.CalculationTaskFactory;
 
 public class HashBreakerFactory implements CalculationTaskFactory
 {
-    String hash = "";
-    long taskSize = 1_000_000_000L;
-    long atomicTaskSize = 10_000_000L;
-    int maxStringLength = 7; //98 tasks
-    char[] characters = "01234567890abcdefghijklmnopqrstuvwxyz".toCharArray();
+    String hash = "7DAF6D81AE80B8930DE2AAF1140B1BE2";
+    long taskSize = 1_000_000L;
+    long atomicTaskSize = 10_000L;
+    int maxStringLength = 6;
+    char[] characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
     //TMP Place to insert config
 
     public HashBreakerFactory() {}
