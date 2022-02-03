3d2ba019b0db11ec92ac654c5ad9ab46497dde8a
Kamil <61463685+K4m1lP@users.noreply.github.com>
2022-02-03 14:43:19 +0100

    Test update

--- src/test/java/pl/edu/agh/calculationp2p/message/body/GiveProgressTest.java
+++ src/test/java/pl/edu/agh/calculationp2p/message/body/GiveProgressTest.java
@@ -1,6 +1,13 @@
 package pl.edu.agh.calculationp2p.message.body;
 
 import org.junit.jupiter.api.Test;
+import pl.edu.agh.calculationp2p.calculationTask.hashBreaking.HashTaskResult;
+import pl.edu.agh.calculationp2p.state.Progress;
+import pl.edu.agh.calculationp2p.state.task.TaskRecord;
+import pl.edu.agh.calculationp2p.state.task.TaskState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,10 +23,23 @@ class GiveProgressTest {
 
     @Test
     void serializeContent() {
-        //TODO:
+
         Body giveProcess = new GiveProgress(null);
 
         String result = "{\"progress\":\"null\"}";
+        assertEquals(result, giveProcess.serializeContent());
+
+    }
+
+    @Test
+    void serializeContent01() {
+
+        Progress progress = new Progress(new ArrayList<>(List.of(new TaskRecord(2, TaskState.Reserved, 2, new HashTaskResult()))));
+
+        Body giveProcess = new GiveProgress(progress);
+
+        String result = "{\"progress\":[{\"task_id\":2,\"state\":\"reserved\",\"owner\":2,\"result\":[]}]}";
+
         assertEquals(result, giveProcess.serializeContent());
 
     }
